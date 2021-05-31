26ac47f916be3a740cba747b06508848953e535e
tptin76 <tptin.76@gmail.com>
2021-06-01 01:44:56 +0700

    install gdrive

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,13 @@
 sudo apt update
 #install ffmpeg tool
 sudo apt install ffmpeg -y
+
+cd ~
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
+
+mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
+
+chmod +x gdrive
+
+sudo install gdrive /usr/local/bin/gdrive
+
