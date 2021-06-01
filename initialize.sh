7c354b52420cfd5b4cb7bc5ea1364abd38669cdd
tptin76 <tptin.76@gmail.com>
2021-06-01 07:25:37 +0700

    credential

--- initialize.sh
+++ initialize.sh
@@ -3,8 +3,8 @@ sudo apt update
 sudo apt install ffmpeg -y
 
 cd ~
-wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
-mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
+wget 'https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download'
+mv 'uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download' gdrive
 chmod +x gdrive
 sudo install gdrive /usr/local/bin/gdrive
 
