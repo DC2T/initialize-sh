2860f75418fe8e89a9fe73de97f5c8a91672e367
tptin76 <tptin.76@gmail.com>
2021-06-01 21:07:46 +0700

    remove install gdrive

--- initialize.sh
+++ initialize.sh
@@ -2,9 +2,4 @@ sudo apt update
 #install ffmpeg tool
 sudo apt install ffmpeg -y
 
-cd ~
-wget 'https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download'
-mv 'uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download' gdrive
-chmod +x gdrive
-sudo install gdrive /usr/local/bin/gdrive
 
