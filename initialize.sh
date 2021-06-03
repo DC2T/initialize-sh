a94602c7a543ae943026fc7cfac93c21ec07bb47
tptin76 <tptin.76@gmail.com>
2021-06-03 22:27:14 +0700

    install rclone google drive

--- initialize.sh
+++ initialize.sh
@@ -2,4 +2,12 @@ sudo apt update
 #install ffmpeg tool
 sudo apt install ffmpeg -y
 
+#install rclone tool
+curl 'https://rclone.org/install.sh' | sudo bash
 
+cd ~/initialize-sh
+#copy file credentials.conf to /home/worker/.config/rclone/rclone.conf
+cp -if credentials.conf ~/.config/rclone/rclone.conf
+
+#check google drvie 
+rclone about gdrive:
