2044e3dabfaf2733df4139fcc69173c321b17d8c
tptin76 <tptin.76@gmail.com>
2021-06-04 21:02:21 +0700

    edit file sh

--- initialize.sh
+++ initialize.sh
@@ -7,7 +7,7 @@ curl 'https://rclone.org/install.sh' | sudo bash
 
 cd ~/initialize-sh
 #make folder rclone
-[! -f ~/.config/rclone] && mkdir -p ~/.config/rclone
+[ ! -f ~/.config/rclone] && mkdir -p ~/.config/rclone
 
 #copy file credentials.conf to /home/worker/.config/rclone/rclone.conf
 yes | cp -rf credentials.conf ~/.config/rclone/rclone.conf
