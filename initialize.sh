6102bfc624ec35f6b9595ae9bed0526cd5f3395b
tptin76 <tptin.76@gmail.com>
2021-06-09 13:32:34 +0700

    update

--- initialize.sh
+++ initialize.sh
@@ -7,10 +7,10 @@ curl 'https://rclone.org/install.sh' | sudo bash
 
 cd ~/initialize-sh
 #make folder rclone
-[ ! -f ~/.config/rclone] && mkdir -p ~/.config/rclone
+[ ! -d ~/.config/rclone] && mkdir ~/.config/rclone
 
-#copy file credentials.conf to /home/worker/.config/rclone/rclone.conf
-yes | cp -rf credentials.conf ~/.config/rclone/rclone.conf
+#copy file rclone.conf to /home/worker/.config/rclone/rclone.conf
+yes | cp -rf rclone.conf ~/.config/rclone
 
 #check google drvie 
 rclone about gdrive:
